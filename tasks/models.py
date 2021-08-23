371a82b7b0d51d75e82e6ac8f6bbfa4f2b922c52
kkuzovov <kkuzovov@mit.ru>
2021-08-23 17:47:40 +0300

    fix: created_at

--- tasks/models.py
+++ tasks/models.py
@@ -58,7 +58,7 @@ class InputData(models.Model):
         (JSON, 'Массив точек в формате JSON'),
     )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Автор входных данных', related_name='tasks_inputdata', on_delete=models.DO_NOTHING)
-    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True, blank=True)
     expected_sat = models.ForeignKey(to=Satellite, verbose_name='Ожидаемый спутник', related_name='tasks_inputdata', null=True, on_delete=models.DO_NOTHING)
     data_type = models.SmallIntegerField('Тип данных', choices=TYPE_CHOICES, default=NONE)
     data_tle = models.TextField('Данные в формате TLE', blank=True)
