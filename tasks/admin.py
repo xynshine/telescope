7f9cd5b720ecc75f103afd5f9c0854aaae1ee15a
kkuzovov <kkuzovov@mit.ru>
2021-09-02 17:36:40 +0300

    empower admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Telescope, Task, TrackingData, TLEData, Point, Frame, TrackPoint, Balance, BalanceRequest, TaskResult
+from .models import Telescope, Task, TrackingData, TLEData, Point, Frame, TrackPoint, Balance, BalanceRequest, \
+    TaskResult, Satellite, InputData
 
 admin.site.register(Telescope)
+admin.site.register(Satellite)
 admin.site.register(Task)
+admin.site.register(InputData)
 admin.site.register(TrackingData)
 admin.site.register(TLEData)
 admin.site.register(Point)
